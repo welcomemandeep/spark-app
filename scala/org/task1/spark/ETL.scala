9d4be6f1f43e042be7602258628255ebecb2ffe9
Mandeep Gandhi <mgandhi@adobe.com>
2019-03-05 10:26:55 +0530

    add more columns for question 3

--- scala/org/task1/spark/ETL.scala
+++ scala/org/task1/spark/ETL.scala
@@ -3,7 +3,6 @@
 
 package org.task1.spark
 
-import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql._
 
 
@@ -15,7 +14,7 @@ object ETL {
     val df = sqlContext.read.format("csv").option("header",true).load(filePath)
     df.select("FlightDate","Carrier","Flights",
       "Origin","Dest","AirlineId","DepTime","DepDelayMinutes",
-      "ArrTime","ArrDelayMinutes","DayOfWeek").write.format("csv")
+      "ArrTime","ArrDelayMinutes","DayOfWeek","FlightNum","UniqueCarrier,"ArrDelay").write.format("csv")
       .option("header",true).save("hdfs:///root/airline_on_time_performance_cleaned_data")
     
     
